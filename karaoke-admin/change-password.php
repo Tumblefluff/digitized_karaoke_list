840b806320d5ff0697f1910a24ccb3425113b1fb
Tumblefluff <88256829+Tumblefluff@users.noreply.github.com>
2025-01-31 23:06:59 -0800

    Update change-password.php

--- karaoke-admin/change-password.php
+++ karaoke-admin/change-password.php
@@ -48,7 +48,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Change Password</title>
-	<link rel="stylesheet" href="style.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h2>Change Password</h2>
